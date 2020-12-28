3590baa25c36e7910b658090ab94b483bc0f8695
Mohit Aggarwal <mohitw@cisco.com>
2020-12-27 23:50:15 -0800

    Added remove duplicate from sorted array problem

---
+++ RemoveDupFromSortedArr.java
@@ -0,0 +1,24 @@
+class RemoveDupFromSortedArr {
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        
+        int i=0,j=1;
+        
+        while (j<nums.length) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+            j++;
+        }
+        
+        return i+1;
+    }
+
+    publilc static void main(String[] args) {
+        int[] arr = {1,5,5,7,9,12,13,13,29,44,44,44,57};
+        System.out.println("Length of sorted inetger array after removing all duplicate elements: " + removeDuplicates(arr));
+    }
+}
