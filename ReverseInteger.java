0af2d295dca312577d335fd5e172a132f8c47818
Mohit Aggarwal <mohitw@cisco.com>
2020-12-27 18:34:41 -0800

    Reverse an Integer

---
+++ ReverseInteger.java
@@ -0,0 +1,28 @@
+class ReverseInteger {
+    public int reverse(int x) {
+        if (x == 0) {
+            return x;
+        }
+
+        LinkedList<Integer> queue = new LinkedList<>();
+        while (x!=0) {
+            queue.addLast(x%10);
+            x/=10;
+        }
+
+        int result = 0;
+        while (!queue.isEmpty()) {
+            int num = queue.removeFirst();
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE/10 && num >7)) {
+                return 0;
+            }
+            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE/10 && num <-8)) {
+                return 0;
+            }
+            result *= 10;
+            result += num;
+        }
+
+        return result;
+    }
+}
