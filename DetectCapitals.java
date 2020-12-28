b35e4eb27db2a5319f185da04f2a23d6dd8357cc
Mohit Aggarwal <mohitw@cisco.com>
2020-12-28 00:29:46 -0800

    Given a word, you need to judge whether the usage of capitals in it is right or not.

---
+++ DetectCapitals.java
@@ -0,0 +1,23 @@
+class DetectCapitals {
+    public static boolean detectCapitalUse(String word) {
+        boolean isCorrectUsage = false;
+        int upperCaseCount = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (Character.isUpperCase(word.charAt(i))) {
+                upperCaseCount++;
+            }
+        }
+        
+        if (upperCaseCount == 0 || upperCaseCount == word.length() || (upperCaseCount == 1 && Character.isUpperCase(word.charAt(0)))) {
+            isCorrectUsage = true;
+        }
+        
+        
+        return isCorrectUsage;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Is capital usage correct? " + detectCapitalUse("floG"));
+    }
+
+}
