bd519437c55b6376dad71f62dddb2b89cfbfdf5a
Mohit Aggarwal <mohitaggarwal@Mohits-MacBook-Pro.local>
2021-02-26 23:39:58 -0800

    Check duplicate chars in a string

--- strings/CheckDuplicates.java
+++ strings/CheckDuplicates.java
@@ -1,4 +1,23 @@
 package strings;
 
 public class CheckDuplicates {
+    boolean containsDuplicate(String str) {
+        boolean result = false;
+        byte[] barr = new byte[256];
+        for (int i=0; i<str.length(); i++) {
+            if(barr[str.charAt(i)] == 0) {
+                barr[str.charAt(i)] = 1;
+            } else {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void main (String[] args) {
+        CheckDuplicates cd = new CheckDuplicates();
+        String str = "blue";
+        System.out.println("This string: \'"+ str +"\' contains duplicates? " + cd.containsDuplicate(str));
+    }
 }
