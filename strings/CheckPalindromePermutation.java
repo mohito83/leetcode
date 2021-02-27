05a317fada0553987bc61d4acba7902aa26718ee
Mohit Aggarwal <mohitaggarwal@Mohits-MacBook-Pro.local>
2021-02-27 15:18:39 -0800

    Check if a given string is a pallindrome permutation

---
+++ strings/CheckPalindromePermutation.java
@@ -0,0 +1,45 @@
+package strings;
+
+import com.sun.tools.javac.comp.Check;
+
+import java.awt.peer.ListPeer;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CheckPalindromePermutation {
+    boolean isPalindromePermutation(String str) {
+        if (str == null || str.length() == 0) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i =0; i<str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                continue;
+            }
+            if (map.containsKey(str.charAt(i))) {
+                int val = map.get(str.charAt(i));
+                map.put(str.charAt(i), ++val);
+            } else {
+                map.put(str.charAt(i), 1);
+            }
+        }
+
+        int count = 0;
+        for (char c : map.keySet()) {
+            int val = map.get(c);
+            if (val %2 ==1){
+                count++;
+            }
+        }
+
+        return count <= 1;
+    }
+
+    public static void main(String[] args) {
+        CheckPalindromePermutation cpc = new CheckPalindromePermutation();
+        String str = "tact coa";
+        System.out.println("Is string: "+str +" palindrome permutation? "+ cpc.isPalindromePermutation(str));
+    }
+}
