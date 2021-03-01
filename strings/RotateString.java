2e4fa0a31d818c1bb9c3bba45f1a2f8cb88dca9d
Mohit Aggarwal <mohitaggarwal@Mohits-MacBook-Pro.local>
2021-02-28 22:44:59 -0800

    String rotation

---
+++ strings/RotateString.java
@@ -0,0 +1,44 @@
+package strings;
+
+public class RotateString {
+
+    boolean isSubString(String str1, String str2) {
+        if (str1 == null || str2 == null) {
+             return false;
+        }
+
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        int i=0, j=0;
+
+        while (j < str2.length()) {
+            if (str1.charAt(i) == str2.charAt(j)) {
+                break;
+            }
+            j++;
+        }
+
+        while (i < str1.length()) {
+            if (str1.charAt(i) != str2.charAt(j)) {
+                return  false;
+            }
+            i++;
+            j++;
+            if (j == str2.length()) {
+                j=0;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        RotateString rs = new RotateString();
+        String str1 = "waterbottle";
+        String str2 = "erbottlewat";
+
+        System.out.println("Are these strings rotation of on e another? " + rs.isSubString(str1, str2));
+    }
+}
